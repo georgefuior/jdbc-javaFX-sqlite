1fb0cd2b80a2784b99b1882756747220b5620626
George Fuior <george.fuior@icloud.com>
2020-08-12 22:04:21 +0300

    JavaSE

---
+++ src/sample/Main.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.Model.Datasource;
+
+import javax.xml.crypto.Data;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.listArtists();
+
+        primaryStage.setTitle("Music Database");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        if(!Datasource.getInstance().open()){
+            System.out.println("FATAL Error: Could not connect to database");
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Datasource.getInstance().close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
